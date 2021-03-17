c6f57938c6fdf1014f9103f6c4e20f53509be7a7
Chaoqun Wei <chaoqunwei@Chaoquns-MacBook-Pro.local>
2021-03-17 22:44:28 +0800

    update: update schema_field collection

--- src/antapi/core/schema/schema.go
+++ src/antapi/core/schema/schema.go
@@ -2,43 +2,6 @@ package schema
 
 import "github.com/gogf/gf/frame/g"
 
-// FieldType .
-type FieldType string
-
-// Supported field types
-const (
-	String    FieldType = "String"
-	Color     FieldType = "Color"
-	Email     FieldType = "Email"
-	Tel       FieldType = "Tel"
-	URL       FieldType = "Url"
-	Password  FieldType = "Password"
-	Text      FieldType = "Text"
-	LongText  FieldType = "LongText"
-	RichText  FieldType = "RichText"
-	Markdown  FieldType = "Markdown"
-	Code      FieldType = "Code"
-	HTML      FieldType = "HTML"
-	Signature FieldType = "Signature"
-	Media     FieldType = "Media"
-	Enum      FieldType = "Enum"
-	JSON      FieldType = "JSON"
-	UUID      FieldType = "UUID"
-	Int       FieldType = "Int"
-	BigInt    FieldType = "BigInt"
-	Float     FieldType = "Float"
-	Money     FieldType = "Money"
-	Date      FieldType = "Date"
-	DateTime  FieldType = "DateTime"
-	Time      FieldType = "Time"
-	TimeStamp FieldType = "TimeStamp"
-	Year      FieldType = "Year"
-	Bool      FieldType = "Bool"
-	Array     FieldType = "Array"
-	BelongsTo FieldType = "BelongsTo"
-	HasMany   FieldType = "HasMany"
-)
-
 // Schema .
 type Schema struct {
 	ID          string
