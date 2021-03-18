10e60a0ac490c72f75e97a8d4e60ee82b2497034
Chaoqun Wei <chaoqunwei@Chaoquns-MacBook-Pro.local>
2021-03-18 17:33:04 +0800

    update

--- src/antapi/core/crud/insert.go
+++ src/antapi/core/crud/insert.go
@@ -10,34 +10,37 @@ import (
 // InsertOne : 插入单个数据
 // TODO: 返回插入数据的uuid
 func InsertOne(collectionName string, data interface{}) error {
-	db := g.DB()
-	oriObj, err := gjson.LoadJson(data)
-	if err != nil {
-		return err
-	}
-	schema, err := model.GetSchema(collectionName)
-	if err != nil {
-		return err
-	}
-
-	newObj := gjson.New(`{}`)
-	for _, field := range schema.Fields {
-		val := oriObj.Get(field.Name)
-		if validErr := field.CheckFieldValue(val); validErr != nil {
-			return validErr.Current()
-		}
-		newObj.Set(field.Name, val)
-	}
-
-	_, err = db.Table(collectionName).Data(newObj.Map()).Insert()
-	if err != nil {
-		return err
-	}
-	return nil
-
+	return InsertList(collectionName, data)
 }
 
 // InsertList : 插入多个数据
-func InsertList() {
+func InsertList(collectionName string, data ...interface{}) error {
+	db := g.DB()
+	allData := make([]map[string]interface{}, 0)
+	for i := 0; i < len(data); i++ {
+		oriObj, err := gjson.LoadJson(data)
+		if err != nil {
+			return err
+		}
+		schema, err := model.GetSchema(collectionName)
+		if err != nil {
+			return err
+		}
 
+		var newObj map[string]interface{}
+		for _, field := range schema.Fields {
+			val := oriObj.Get(field.Name)
+			if validErr := field.CheckFieldValue(val); validErr != nil {
+				return validErr.Current()
+			}
+			newObj[field.Name] = val
+		}
+
+		allData = append(allData, newObj)
+	}
+
+	if _, err := db.Table(collectionName).Insert(allData); err != nil {
+		return err
+	}
+	return nil
 }
