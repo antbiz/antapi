0c09e8e5904fd8affb8ca3a5d748e2ed9c998fcb
Chaoqun Wei <chaoqunwei@Chaoquns-MacBook-Pro.local>
2021-03-18 22:47:33 +0800

    update

--- src/antapi/core/crud/delete.go
+++ src/antapi/core/crud/delete.go
@@ -1,12 +1,36 @@
 package crud
 
-import "github.com/gogf/gf/frame/g"
+import (
+	"antapi/model"
+
+	"github.com/gogf/gf/frame/g"
+)
 
 // Delete : 删除指定数据
 func Delete(collectionName string, where interface{}, args ...interface{}) error {
 	db := g.DB()
-	if _, err := db.Table(collectionName).Where(where, args...).Delete(); err != nil {
+	schema, err := model.GetSchema(collectionName)
+
+	// 查询需要删除的id
+	var delIds []string
+	values, err := db.Table(collectionName).Where(where, args...).Array("id")
+	if err != nil {
 		return err
 	}
+	for _, val := range values {
+		delIds = append(delIds, val.String())
+	}
+
+	// 删除主体数据
+	if _, err := db.Table(collectionName).Where("id", delIds).Delete(); err != nil {
+		return err
+	}
+	// 删除子表数据
+	for _, field := range schema.GetTableFields() {
+		if _, err := db.Table(field.RelatedCollection).Where("pid", delIds).Where("pfd", field.Name).Where("pct", collectionName).Delete(); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
