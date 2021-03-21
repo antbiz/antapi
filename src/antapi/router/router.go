03526cb8d102736d606959c80feed9280ca30c94
BeanWei <bean0wei@dingtalk.com>
2021-03-21 20:30:17 +0800

    update

--- src/antapi/router/router.go
+++ src/antapi/router/router.go
@@ -1,14 +1,24 @@
 package router
 
-// import (
-// 	"antapi/app/api"
-// 	"github.com/gogf/gf/frame/g"
-// 	"github.com/gogf/gf/net/ghttp"
-// )
+import (
+	"antapi/api/controller"
 
-// func init() {
-// 	s := g.Server()
-// 	s.Group("/", func(group *ghttp.RouterGroup) {
-// 		group.ALL("/hello", api.Hello)
-// 	})
-// }
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
+)
+
+func init() {
+	s := g.Server()
+	s.Group("/api/business", func(group *ghttp.RouterGroup) {
+		// 查询 {collection} 列表
+		group.GET("/{collection}", controller.CRUD.GetList)
+		// 获取 {collection} 详情
+		group.GET("/{collection}/{id}", controller.CRUD.Get)
+		// 添加 {collection}
+		group.POST("/{collection}/{id}", controller.CRUD.Create)
+		// 修改 {collection}
+		group.PATCH("/{collection}/{id}", controller.CRUD.Update)
+		// 删除 {collection}
+		group.DELETE("/{collection}/{id}", controller.CRUD.Delete)
+	})
+}
