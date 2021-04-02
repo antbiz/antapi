98fc6f557df95ce5c31bd526d1bf16eced05d2d7
Chaoqun Wei <chaoqunwei@Chaoquns-MacBook-Pro.local>
2021-04-02 22:07:10 +0800

    update

--- src/antapi-fe/src/pages/project/schema/index.tsx
+++ src/antapi-fe/src/pages/project/schema/index.tsx
@@ -1,7 +1,6 @@
 import { PlusOutlined } from '@ant-design/icons';
-import { Button, Divider, Popconfirm, message, Modal} from 'antd';
+import { Button, Popconfirm, message, Modal} from 'antd';
 import React, { useRef, useState } from 'react';
-import { Link } from 'umi';
 import { PageContainer } from '@ant-design/pro-layout';
 import type { ProColumns, ActionType } from '@ant-design/pro-table';
 import ProTable from '@ant-design/pro-table';
@@ -33,19 +32,19 @@ const handleAdd = async (schema: API.Schema) => {
  *
  * @param fields
  */
-// const handleUpdate = async (schema: API.Schema) => {
-//   const hide = message.loading('正在更新');
-//   try {
-//     await updateRule({...schema});
-//     hide();
-//     message.success('更新成功');
-//     return true;
-//   } catch (error) {
-//     hide();
-//     message.error('更新失败请重试！');
-//     return false;
-//   }
-// };
+const handleUpdate = async (schema: API.Schema) => {
+  const hide = message.loading('正在更新');
+  try {
+    await updateRule({...schema});
+    hide();
+    message.success('更新成功');
+    return true;
+  } catch (error) {
+    hide();
+    message.error('更新失败请重试！');
+    return false;
+  }
+};
 
 /**
  * 删除
@@ -69,11 +68,11 @@ const handleRemove = async (id: string) => {
 
 const SchemaList: React.FC = () => {
   /** 新建窗口的弹窗 */
-  const [createModalVisible, handleModalVisible] = useState<boolean>(false);
-  /** 更新窗口的弹窗 */
-  // const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
+  const [editModalVisible, handleEditModalVisible] = useState<boolean>(false);
+  const [currentItem, setCurrentItem] = useState<API.Schema>({});
 
   const actionRef = useRef<ActionType>();
+  const genRef = useRef();
   const projectId = getProjectId();
 
   const columns: ProColumns<API.Schema>[] = [
@@ -90,14 +89,6 @@ const SchemaList: React.FC = () => {
       dataIndex: 'option',
       valueType: 'option',
       render: (_, record) => [
-        <a
-          key="update"
-          onClick={() => {
-            handleModalVisible(true);
-          }}
-        >
-          更新
-        </a>,
         <Popconfirm
           key="delete"
           title="确定删除？"
@@ -109,6 +100,15 @@ const SchemaList: React.FC = () => {
         >
           <a href="#">删除</a>
         </Popconfirm>,
+        <a
+          key="update"
+          onClick={() => {
+            setCurrentItem(record);
+            handleEditModalVisible(true);
+          }}
+        >
+          更新
+        </a>,
       ],
     },
   ];
@@ -126,7 +126,7 @@ const SchemaList: React.FC = () => {
             type="primary"
             key="primary"
             onClick={() => {
-              handleModalVisible(true);
+              handleEditModalVisible(true);
             }}
           >
             <PlusOutlined />
@@ -136,24 +136,22 @@ const SchemaList: React.FC = () => {
         params={{ projectId }}
         request={getSchemas}
         columns={columns}
-        rowSelection={{
-          onChange: (_, selectedRows) => {
-            setSelectedRows(selectedRows);
-          },
-        }}
       />
       <Modal
-        title="新建模型"
+        title={currentItem?.id ? "更新模型" : "新建模型"}
         width="90%"
         bodyStyle={{ height: '70vh'}}
-        visible={createModalVisible}
+        maskClosable={false}
+        visible={editModalVisible}
         cancelText="取消"
         okText="保存"
-        onCancel={() => handleModalVisible(false)}
-        onOk={async (value) => {
-          const success = await handleAdd(value as API.Schema);
+        onCancel={() => handleEditModalVisible(false)}
+        onOk={async () => {
+          const value = genRef.current.getValue();
+          console.log(value)
+          const success = value?.id ? await handleUpdate(value as API.Schema) : await handleAdd(value as API.Schema);
           if (success) {
-            handleModalVisible(false);
+            handleEditModalVisible(false);
             if (actionRef.current) {
               actionRef.current.reload();
             }
@@ -161,7 +159,9 @@ const SchemaList: React.FC = () => {
         }}
       >
         <Generator
+          ref={genRef}
           extraButtons={[true, true, false, false]}
+          // defaultValue={currentItem}
         />
       </Modal>
     </PageContainer>
