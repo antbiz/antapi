0aa70bf2d70091e35d48faeee835c93c5e3ecbd6
Chaoqun Wei <chaoqunwei@Chaoquns-MacBook-Pro.local>
2021-05-22 19:00:02 +0800

    fix

--- internal/app/service/user.go
+++ internal/app/service/user.go
@@ -26,7 +26,7 @@ func (srv *userSrv) EncryptPwd(username, password string) string {
 // GetUserByLogin 根据 用户名/手机号/邮箱 查询用户信息
 func (srv *userSrv) GetUserByLogin(ctx context.Context, login string) (*gjson.Json, error) {
 	return dao.Get(ctx, srv.CollectionName(), &dao.GetOptions{
-		Filter:              bson.D{{"$or", bson.D{{"username", login}, {"phone", login}, {"email", login}}}},
+		Filter:              bson.M{"$or": []bson.M{{"username": login}, {"phone": login}, {"email": login}}},
 		IncludeHiddenField:  true,
 		IncludePrivateField: true,
 	})
